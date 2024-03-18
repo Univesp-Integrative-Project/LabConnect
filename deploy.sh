d6ec5a76c4b0e3c4648cddd1f5d86e54c71366bf
Sérgio Magno <sergiomagno@outlook.com>
2024-03-18 18:12:04 -0300

    correção do fluxo

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+# Clonar o repositório da aplicação Django
+git clone https://github.com/Univesp-Integrative-Project/LabConnect.git
+
+# Entrar no diretório da aplicação Django
+cd LabConnect
+
+# Construir e iniciar os contêineres Docker
+docker-compose up -d
