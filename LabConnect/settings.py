00a6cee78fd356e2bdbeff86a1acf9e7db74f1c4
Sérgio Magno <sergiomagno@outlook.com>
2024-03-19 21:46:20 -0300

    rollback

--- LabConnect/settings.py
+++ LabConnect/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CORS_ALLOWED_ORIGINS = ['https://18.116.89.141']
+#CORS_ALLOWED_ORIGINS = ['https://18.116.89.141']
 
 # Application definition
 
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'corsheaders',
+    #'corsheaders',
 
     'Agenda',
 ]
