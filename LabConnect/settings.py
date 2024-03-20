773389905ff1661efb775674f4a7cc0634dbd1b4
Sérgio Magno <57957273+magnosergio@users.noreply.github.com>
2024-03-20 10:04:38 -0300

    Update settings.py

--- LabConnect/settings.py
+++ LabConnect/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-#CORS_ALLOWED_ORIGINS = ['https://18.116.89.141']
+CSRF_TRUSTED_ORIGINS = ['https://18.116.89.141']
 
 # Application definition
 
@@ -38,8 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    #'corsheaders',
 
     'Agenda',
 ]
